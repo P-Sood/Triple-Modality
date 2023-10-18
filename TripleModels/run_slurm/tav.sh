dbcd744413d32adcf27d63580a2ec8c3fe8b2a00
Pranav Sood <55671093+P-Sood@users.noreply.github.com>
2023-10-18 10:27:09 -0700

    new sbatch commands

--- TripleModels/run_slurm/tav.sh
+++ TripleModels/run_slurm/tav.sh
@@ -4,19 +4,17 @@
 
 # Give job a name
 
-#SBATCH --time 03-12:00 # time (DD-HH:MM)
+#SBATCH --time 00-12:00 # time (DD-HH:MM)
 
 #SBATCH --nodes=1
 
-#SBATCH --gpus-per-node=v100l:2 # request GPU
+#SBATCH --gres=gpu:1 # request GPU
 
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks=1
 
 #SBATCH --cpus-per-task=6 # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
 
 #SBATCH --mem=180G # memory per node
-
-#SBATCH --account=def-whkchun # Runs it on the dedicated nodes we have
 
 #SBATCH --output=/scratch/prsood/tav_mae/logs/%N-%j.out # %N for node name, %j for jobID # Remember to mae logs-dir
 
@@ -27,5 +25,4 @@ module load cuda
 module load cudnn/8.0.3
 
 source /home/prsood/projects/def-whkchun/prsood/sarcasm_venv/bin/activate
-parallel --joblog /scratch/prsood/tav_mae/logs/parallel.log < ./meld_multi.txt
 # wandb agent ddi/MAEncoder_7Emo_Test/v88xjdsy
