cec2555758fbf9a10b4a0c316ef42e5b11783b44
Pranav Sood <55671093+P-Sood@users.noreply.github.com>
2023-11-07 16:23:20 -0800

    removed files we dont need, and unnecesary lines

--- SingleModels/models/text.py
+++ SingleModels/models/text.py
@@ -1,5 +1,3 @@
-import torch
-from torch import nn
 from transformers import AutoModel
 import torch.nn as nn
 import pdb
---
+++ TripleModels/run_slurm/meld.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Give job a name
+
+#SBATCH --time 00-12:00 # time (DD-HH:MM)
+#SBATCH -p gpu
+#SBATCH -q gpu-8
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6 # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
+#SBATCH --output=../outputs/meld_triple.txt
+
+wandb agent ddi/MeldTriple/jtzs474d
