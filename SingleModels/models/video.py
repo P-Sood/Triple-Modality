39f92d20b969282e969bd136ba7d000812621f38
Pranav Sood <55671093+P-Sood@users.noreply.github.com>
2023-11-18 19:42:29 -0800

    rm bool_masked_pos in videoMAE, so they are all same seq_len

--- SingleModels/models/video.py
+++ SingleModels/models/video.py
@@ -25,26 +25,8 @@ def collate_batch(batch, must):  # batch is a pseudo pandas array of two columns
             video_context.append(input[2])
         label_list.append(label)
 
-    batch_size = len(label_list)
-    vid_mask = torch.randint(
-        -13, 2, (batch_size, 1568)
-    )  # 8*14*14 = 1568 is just the sequence length of every video with VideoMAE, so i just hardcoded it,
-    vid_mask[vid_mask > 0] = 0
-    vid_mask = vid_mask.bool()
-    # now we have a random mask over values so it can generalize better
-    x = torch.count_nonzero(vid_mask.int()).item()
-    rem = (1568 * batch_size - x) % batch_size
-    if rem != 0:
-        idx = torch.where(vid_mask.view(-1) == 0)[
-            0
-        ]  # get all indicies of 0 in flat tensor
-        num_to_change = rem  # as follows from example abow
-        idx_to_change = choice(idx, size=num_to_change, replace=False)
-        vid_mask.view(-1)[idx_to_change] = 1
-
     visual_embeds = {
         "video_embeds": torch.stack(video_list).permute(0, 2, 1, 3, 4),
-        "visual_mask": vid_mask,
         "video_context": torch.stack(video_context).permute(0, 2, 1, 3, 4),
     }
 
@@ -72,20 +54,18 @@ class VideoClassification(nn.Module):
         self.dropout = nn.Dropout(self.dropout)
         self.linear1 = nn.Linear(1024, self.output_dim)
 
-    def forward(self, video_embeds, video_context, visual_mask, check="train"):
-        vid_outputs = self.videomae(video_embeds, visual_mask)[0][:, 0] 
+    def forward(self, video_embeds, video_context, check="train"):
+        vid_outputs = self.videomae(video_embeds, None)[0][:, 0] 
         vid_outputs = vid_outputs[:, 0] 
         # take the first token now it has 2 dimensions
         del video_embeds
     
 
         if self.must:
-            vid_context = self.videomae(video_context, visual_mask)[0]
+            vid_context = self.videomae(video_context, None)[0]
             vid_context = vid_context[:, 0] 
             del video_context
             vid_outputs = (vid_outputs * self.p + vid_context * (1 - self.p)) / 2
-
-        del visual_mask
 
         if check == "train":
             vid_outputs = self.dropout(vid_outputs)
