a95a682827b0db9279e79237a2bc3af57095dca4
Pranav Sood <55671093+P-Sood@users.noreply.github.com>
2023-11-20 10:07:13 -0800

    fixed concat

--- concat.py
+++ concat.py
@@ -15,9 +15,9 @@ def audio():
             # Copy each dataset to the output file
             f2.copy(name, fo)
 def video():
-    vf1 = 'meld_videos_blackground.hdf5'
-    vf2 = 'iemo_videos_blackground.hdf5'
-    output_vf = 'meld_iemo_videos_blackground.hdf5'
+    vf1 = 'data/meld_videos_blackground.hdf5'
+    vf2 = 'data/iemo_videos_blackground.hdf5'
+    output_vf = 'data/meld_iemo_videos_blackground.hdf5'
 
     with h5py.File(vf1, 'r' , libver="latest", swmr=True) as f1, h5py.File(vf2, 'r' , libver="latest", swmr=True) as f2, h5py.File(output_vf, 'w' , libver="latest", swmr=True) as fo:
         # Iterate over datasets in the first file
