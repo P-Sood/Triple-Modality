52df5f97bb882dc9902d59a1c242f0a2b4520afe
Pranav Sood <55671093+P-Sood@users.noreply.github.com>
2023-11-20 15:17:51 -0800

    first 512 tokens

--- SingleModels/models/whisper.py
+++ SingleModels/models/whisper.py
@@ -61,11 +61,11 @@ class WhisperForEmotionClassification(nn.Module):
         self.linear1 = nn.Linear(1024, self.output_dim)
 
     def forward(self, audio_features, context_audio, check):
-        aud_outputs = self.whisper.encoder(audio_features)[0]
+        aud_outputs = self.whisper.encoder(audio_features)[0][:,:512,:]
         aud_outputs = aud_outputs.mean(dim=1)
         del audio_features
         if self.must:
-            aud_context = self.whisper(context_audio).hidden_states.mean(dim=1)
+            aud_context = self.whisper.encoder(context_audio)[0][:,:512,:]
             aud_context = aud_context.mean(dim=1)
             del context_audio
             aud_outputs = (aud_outputs * self.p + aud_context * (1 - self.p)) / 2
