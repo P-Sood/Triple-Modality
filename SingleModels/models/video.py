e68db71ff8b57a59b37d45d15abb693f63e91782
Pranav Sood <55671093+P-Sood@users.noreply.github.com>
2023-11-20 17:25:00 -0800

    now video gives us a seq_len of 512 too

--- SingleModels/models/video.py
+++ SingleModels/models/video.py
@@ -25,9 +25,18 @@ def collate_batch(batch, must):  # batch is a pseudo pandas array of two columns
             video_context.append(input[2])
         label_list.append(label)
 
+    batch_size = len(label_list)
+    vid_mask = torch.zeros(batch_size, 1568).bool()
+    idx = torch.arange(1568)
+    idx_to_change = torch.Tensor(choice(idx, size=1056, replace=False))
+    # Repeat this tensor batch_size times
+    idx_to_change = idx_to_change.repeat(batch_size, 1).long()
+    vid_mask[:, idx_to_change] = True
+
     visual_embeds = {
         "video_embeds": torch.stack(video_list).permute(0, 2, 1, 3, 4),
         "video_context": torch.stack(video_context).permute(0, 2, 1, 3, 4),
+        "video_mask": vid_mask,
     }
 
     return visual_embeds, torch.Tensor(np.array(label_list)).long()
@@ -54,15 +63,15 @@ class VideoClassification(nn.Module):
         self.dropout = nn.Dropout(self.dropout)
         self.linear1 = nn.Linear(1024, self.output_dim)
 
-    def forward(self, video_embeds, video_context, check="train"):
-        vid_outputs = self.videomae(video_embeds, None)[0]
+    def forward(self, video_embeds, video_mask, video_context, check="train"):
+        vid_outputs = self.videomae(video_embeds, bool_masked_pos = video_mask)[0]
         vid_outputs = vid_outputs[:, 0] 
         # take the first token now it has 2 dimensions
         del video_embeds
     
 
         if self.must:
-            vid_context = self.videomae(video_context, None)[0]
+            vid_context = self.videomae(video_context, bool_masked_pos = video_mask)[0]
             vid_context = vid_context[:, 0] 
             del video_context
             vid_outputs = (vid_outputs * self.p + vid_context * (1 - self.p)) / 2
