2efa02f5801862b15692f0ed3c841367a614a9ff
Pranav Sood <55671093+P-Sood@users.noreply.github.com>
2023-11-21 22:44:25 -0800

    Going for gold

--- TripleModels/run_slurm/meld.sh
+++ TripleModels/run_slurm/meld.sh
@@ -2,14 +2,13 @@
 
 # Give job a name
 
-#SBATCH --time 00-12:00 # time (DD-HH:MM)
 #SBATCH -p gpu
 #SBATCH -q gpu-8
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=6 # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
-#SBATCH --output=../outputs/meld_triple.txt
+#SBATCH --output=../outputs/meld_fusion.txt
 
-#wandb agent ddi/MeldTriple/jtzs474d
-wandb agent ddi/MeldOneLoss/unx4sx7l
+wandb agent ddi/Fusion/0y8z5jjv
+# nohup bash -c "sleep 10; git stash; git checkout NewFeatures; sleep 10; git pull; sleep 10; sbatch meld.sh" & disown
