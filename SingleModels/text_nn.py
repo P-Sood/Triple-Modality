ba12baca0524b38b7eb8716c8b0ce055ebbbd768
Pranav Sood <55671093+P-Sood@users.noreply.github.com>
2023-11-23 17:31:17 -0800

    seed function

--- SingleModels/text_nn.py
+++ SingleModels/text_nn.py
@@ -15,7 +15,21 @@ import numpy as np
 from torch.utils.data import DataLoader
 from utils.global_functions import arg_parse, Metrics, MySampler, NewCrossEntropyLoss
 
+import random
+from transformers import set_seed as transformers_set_seed
 
+def set_seed(seed_value):
+    """Set seed for reproducibility."""
+    np.random.seed(seed_value)
+    random.seed(seed_value)
+    torch.manual_seed(seed_value)
+    
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed_value)
+        torch.cuda.manual_seed_all(seed_value)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+    transformers_set_seed(seed_value)
 TESTING_PIPELINE = False
 
 def prepare_dataloader(
@@ -194,6 +208,7 @@ def main():
 
     np.random.seed(config.seed)
     torch.random.manual_seed(config.seed)
+    set_seed(config.seed)
     param_dict = {
         "epoch": config.epoch,
         "patience": config.patience,
