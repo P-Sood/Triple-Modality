140596b20f3ad34e0f8cef1ce4ee849e9fb81866
Z <z@zeerak.org>
2023-11-29 22:51:15 +0400

    Random Seed for 3-Modal

---
+++ TripleModels/run_slurm/Must_Fusion.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Give job a name
+
+#SBATCH -p gpu
+#SBATCH -q gpu-8
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6 # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
+#SBATCH --output=../outputs/iemo_fusion.txt
+
+wandb agent ddi/MustFusion/nm8f1nqv --count 4
+
