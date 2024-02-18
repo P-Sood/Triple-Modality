b366d9dacb3416c03496652f9f76a5ffe76ed5f0
Z <z@zeerak.org>
2024-02-18 10:10:30 +0400

    Make trainer work for whichever metric we use for early_stopping

--- utils/trainer.py
+++ utils/trainer.py
@@ -19,7 +19,7 @@ class Trainer:
     """ big_batch: if batch > big_batch then we will use checkpointing to save memory.
         num_steps: number of steps to divide an epoch into. Used in conjunction with patience for early stopping
     """
-    def __init__(self , big_batch: int , num_steps : int , LOSS : bool = False ) -> None:
+    def __init__(self , big_batch: int , num_steps : int , early_stop : str = "f1" ) -> None:
         
         self.big_batch = big_batch
         self.num_steps = num_steps
@@ -27,7 +27,7 @@ class Trainer:
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.patient_iter = 0    
         
-        self.LOSS = LOSS
+        self.early_stop = early_stop
     
     def get_statistics(self , 
         input: dict, label: np.array, model, criterion, Metric, check="train", epoch=None
@@ -99,7 +99,7 @@ class Trainer:
         patience,
         Metric,
         prev_val_loss,
-        prev_f1,
+        prev_metric,
         total_loss_train,
         iters,
         log_val,
@@ -151,7 +151,7 @@ class Trainer:
                 except StopIteration:
                     break
 
-            prev_val_loss, prev_f1 = self.run_validation(
+            prev_val_loss, prev_metric = self.run_validation(
                 epoch,
                 val_dataloader,
                 model,
@@ -160,7 +160,7 @@ class Trainer:
                 scheduler,
                 Metric,
                 prev_val_loss,
-                prev_f1,
+                prev_metric,
                 total_loss_train / iters,
                 batch_idx,
                 log_val,
@@ -170,7 +170,7 @@ class Trainer:
                 break
             # Do logging every log_val steps
 
-        return model, optimizer, criterion, prev_val_loss, prev_f1
+        return model, optimizer, criterion, prev_val_loss, prev_metric
 
 
     def not_grad_accum(self , 
@@ -185,7 +185,7 @@ class Trainer:
         patience,
         Metric,
         prev_val_loss,
-        prev_f1,
+        prev_metric,
         total_loss_train,
         iters,
         log_val,
@@ -228,7 +228,7 @@ class Trainer:
                     model.zero_grad()
                 except StopIteration:
                     break
-            prev_val_loss, prev_f1 = self.run_validation(
+            prev_val_loss, prev_metric = self.run_validation(
                 epoch,
                 val_dataloader,
                 model,
@@ -237,7 +237,7 @@ class Trainer:
                 scheduler,
                 Metric,
                 prev_val_loss,
-                prev_f1,
+                prev_metric,
                 total_loss_train / iters,
                 batch_idx,
                 log_val,
@@ -245,24 +245,51 @@ class Trainer:
             )
             if self.patient_iter == patience:
                 break
-        return model, optimizer, criterion, prev_val_loss, prev_f1
+        return model, optimizer, criterion, prev_val_loss, prev_metric
 
-    def early_stopping(self , weightedF1, prev_f1, val_loss, prev_val_loss , LOSS = False):
-        if LOSS == False:
-            if weightedF1 >= prev_f1:
-                prev_f1 = weightedF1
-                save = True
-                print(
-                    f"we have seen f1 increase the previous best and we are updating our model" ,  flush = True
+
+    def run_validation(self , 
+        epoch,
+        val_dataloader,
+        model,
+        criterion,
+        optimizer,
+        scheduler,
+        Metric,
+        prev_val_loss,
+        prev_metric,
+        curr_train_loss,
+        step,
+        log_val,
+        path,
+    ):
+        self.log(Metric, curr_train_loss, "train")
+        val_loss, curr_metric = self.validate(val_dataloader, model, criterion, Metric, name="val")
+        prev_val_loss, prev_metric, save = self.early_stopping(curr_metric, prev_metric, val_loss, prev_val_loss , LOSS = False)
+        if save:
+            save_model(model, optimizer, criterion, scheduler, epoch, step, path, log_val)
+        return prev_val_loss, prev_metric
+
+    def validate(self , val_dataloader, model, criterion, Metric, name="val"):
+        total_loss_val = 0
+        with torch.no_grad():
+            for val_input, val_label in tqdm(val_dataloader, desc=name):
+                val_batch_loss = self.get_statistics(
+                    val_input, val_label, model, criterion, Metric, name, epoch=None
                 )
-                self.patient_iter = 0
-            else:
-                save = False
-                self.patient_iter += 1
-                print(
-                    f"we have seen weighted-f1 decrease  for {self.patient_iter} steps and current f1 is {weightedF1}, and previous best f1 loss is {prev_f1}" , flush = True
-                )
-        else:
+                if criterion is not None:
+                    total_loss_val += val_batch_loss.item()
+                del val_input
+                del val_label
+            curr_metric = self.log(
+                Metric,
+                total_loss_val / len(val_dataloader) if criterion is not None else 0,
+                name,
+            )
+        return total_loss_val / len(val_dataloader), curr_metric
+
+    def early_stopping(self , curr_metric, prev_metric, val_loss, prev_val_loss , early_stop = "f1"):
+        if early_stop == "loss":
             if val_loss <= prev_val_loss:
                 print(
                     f"we have seen loss decrease the previous best and we are updating our model" ,  flush = True
@@ -276,48 +303,21 @@ class Trainer:
                 print(
                     f"we have seen loss increase for {self.patient_iter} steps and validation loss is {val_loss}, and previous best validation loss is {prev_val_loss}" , flush = True
                 )
-        return prev_val_loss, prev_f1, save
-
-    def run_validation(self , 
-        epoch,
-        val_dataloader,
-        model,
-        criterion,
-        optimizer,
-        scheduler,
-        Metric,
-        prev_val_loss,
-        prev_f1,
-        curr_loss,
-        step,
-        log_val,
-        path,
-    ):
-        self.log(Metric, curr_loss, "train")
-        val_loss, weightedF1 = self.validate(val_dataloader, model, criterion, Metric, name="val")
-        prev_val_loss, prev_f1, save = self.early_stopping(weightedF1, prev_f1, val_loss, prev_val_loss , LOSS = False)
-        if save:
-            save_model(model, optimizer, criterion, scheduler, epoch, step, path, log_val)
-        return prev_val_loss, prev_f1
-
-
-    def validate(self , val_dataloader, model, criterion, Metric, name="val"):
-        total_loss_val = 0
-        with torch.no_grad():
-            for val_input, val_label in tqdm(val_dataloader, desc=name):
-                val_batch_loss = self.get_statistics(
-                    val_input, val_label, model, criterion, Metric, name, epoch=None
+        else:
+            if curr_metric >= prev_metric:
+                prev_metric = curr_metric
+                save = True
+                print(
+                    f"we have seen {early_stop} increase the previous best and we are updating our model" ,  flush = True
                 )
-                if criterion is not None:
-                    total_loss_val += val_batch_loss.item()
-                del val_input
-                del val_label
-            weightedF1 = self.log(
-                Metric,
-                total_loss_val / len(val_dataloader) if criterion is not None else 0,
-                name,
-            )
-        return total_loss_val / len(val_dataloader), weightedF1
+                self.patient_iter = 0
+            else:
+                save = False
+                self.patient_iter += 1
+                print(
+                    f"we have seen {early_stop} decrease for {self.patient_iter} steps and current {early_stop} is {curr_metric}, and previous best metric is {prev_metric}" , flush = True
+                )
+        return prev_val_loss, prev_metric, save
 
 
     def one_epoch(self , 
@@ -333,7 +333,7 @@ class Trainer:
         patience,
         Metric,
         prev_val_loss,
-        prev_f1,
+        prev_metric,
     ):
         total_loss_train = 0
         iters1 = len(train_dataloader[0])
@@ -347,7 +347,7 @@ class Trainer:
         if sampler == 'Both':
             # Do both
             if epoch % epoch_switch == 0:
-                model, optimizer, criterion, prev_val_loss, prev_f1 = self.not_grad_accum(
+                model, optimizer, criterion, prev_val_loss, prev_metric = self.not_grad_accum(
                     epoch,
                     train_dataloader[0],
                     val_dataloader,
@@ -359,14 +359,14 @@ class Trainer:
                     patience,
                     Metric,
                     prev_val_loss,
-                    prev_f1,
+                    prev_metric,
                     total_loss_train,
                     iters1,
                     log_val1,
                     path,
                 )
             else:
-                model, optimizer, criterion, prev_val_loss, prev_f1 = self.grad_accum(
+                model, optimizer, criterion, prev_val_loss, prev_metric = self.grad_accum(
                     epoch,
                     train_dataloader[1],
                     val_dataloader,
@@ -378,7 +378,7 @@ class Trainer:
                     patience,
                     Metric,
                     prev_val_loss,
-                    prev_f1,
+                    prev_metric,
                     total_loss_train,
                     iters2,
                     log_val2,
@@ -386,7 +386,7 @@ class Trainer:
                 )
         elif sampler == 'Weighted' or sampler == 'Iterative':
             # Do either weightedSampling or iterativeSampling
-            model, optimizer, criterion, prev_val_loss, prev_f1 = self.not_grad_accum(
+            model, optimizer, criterion, prev_val_loss, prev_metric = self.not_grad_accum(
                     epoch,
                     train_dataloader[0] if sampler == 'Weighted' else train_dataloader[1],
                     val_dataloader,
@@ -398,7 +398,7 @@ class Trainer:
                     patience,
                     Metric,
                     prev_val_loss,
-                    prev_f1,
+                    prev_metric,
                     total_loss_train,
                     iters1,
                     log_val1,
@@ -406,7 +406,7 @@ class Trainer:
                 )
         elif sampler == "Both_NoAccum":
             if epoch % epoch_switch == 0:
-                model, optimizer, criterion, prev_val_loss, prev_f1 = self.not_grad_accum(
+                model, optimizer, criterion, prev_val_loss, prev_metric = self.not_grad_accum(
                     epoch,
                     train_dataloader[0],
                     val_dataloader,
@@ -418,14 +418,14 @@ class Trainer:
                     patience,
                     Metric,
                     prev_val_loss,
-                    prev_f1,
+                    prev_metric,
                     total_loss_train,
                     iters1,
                     log_val1,
                     path,
                 )
             else:
-                model, optimizer, criterion, prev_val_loss, prev_f1 = self.not_grad_accum(
+                model, optimizer, criterion, prev_val_loss, prev_metric = self.not_grad_accum(
                     epoch,
                     train_dataloader[1],
                     val_dataloader,
@@ -437,14 +437,14 @@ class Trainer:
                     patience,
                     Metric,
                     prev_val_loss,
-                    prev_f1,
+                    prev_metric,
                     total_loss_train,
                     iters2,
                     log_val2,
                     path,
                 )
         elif sampler == "Iter_Accum":
-            model, optimizer, criterion, prev_val_loss, prev_f1 = self.grad_accum(
+            model, optimizer, criterion, prev_val_loss, prev_metric = self.grad_accum(
                     epoch,
                     train_dataloader[1],
                     val_dataloader,
@@ -456,14 +456,14 @@ class Trainer:
                     patience,
                     Metric,
                     prev_val_loss,
-                    prev_f1,
+                    prev_metric,
                     total_loss_train,
                     iters2,
                     log_val2,
                     path,
                 )
 
-        return model, optimizer, criterion, scheduler, prev_val_loss, prev_f1
+        return model, optimizer, criterion, scheduler, prev_val_loss, prev_metric
 
 
     def train_network(self , 
@@ -490,7 +490,7 @@ class Trainer:
             optimizer, T_0=T_max
         )  # To prevent fitting to local minima != global minima
         prev_val_loss = 100
-        prev_f1 = 0
+        prev_metric = 0
         path = "/".join(os.getcwd().split("/")[:-3]) + "/TAV_Train"
         if checkpoint is not None:
             # epoch_num = checkpoint['epoch']
@@ -500,7 +500,7 @@ class Trainer:
             wandb.log({"epoch": epoch_num, "learning_rate": scheduler.get_last_lr()[0]})
             optimizer.zero_grad()  # Zero out gradients before each epoch.
 
-            model, optimizer, criterion, scheduler, prev_val_loss, prev_f1 = self.one_epoch(
+            model, optimizer, criterion, scheduler, prev_val_loss, prev_metric = self.one_epoch(
                 epoch_num,
                 train_dataloader,
                 val_dataloader,
@@ -513,7 +513,7 @@ class Trainer:
                 patience,
                 Metric,
                 prev_val_loss,
-                prev_f1,
+                prev_metric,
             )
             if self.patient_iter == patience:
                 break
@@ -521,8 +521,8 @@ class Trainer:
         return model
 
 
-    def evaluate(self , model, test_dataloader, Metric):
-        self.validate(test_dataloader, model, None, Metric, name="test")
+    def evaluate(self , model, test_dataloader, Metric, name="test"):
+        self.validate(test_dataloader, model, None, Metric, name)
 
 
     def log(self , Metric, loss, check="train"):
@@ -549,4 +549,4 @@ class Trainer:
         print(f"\n in {check} \n Confusion Matrix = \n{_} \n", flush=True)
         wandb.log({**d1, **multiF1, **multiRec, **multiPrec, **multiAcc})
         Metric.reset_metrics()
-        return F1Weighted
+        return F1Weighted if self.early_stop == "f1" else Acc
