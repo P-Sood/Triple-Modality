71a844b8b0db5cc9f068886400c130fb93e8bfae
Pranav Sood <55671093+P-Sood@users.noreply.github.com>
2024-03-03 01:46:02 -0800

    New Zeerak3

--- TripleModels/run_zeerak3/d_c.sh
+++ TripleModels/run_zeerak3/d_c.sh
@@ -11,13 +11,5 @@
 #SBATCH --output=../outputs/iemo_fusion.txt
 
 
-for i in 1 2 3 4 305
-do
-    python ../tav_nn.py --fusion "d_c" --sampler "Both"  --seed $i --clip 5  --T_max 3 --epoch 7 --dataset "../../data/more_text_IEMO" --dropout 0.6 --patience 14 --batch_size 24 --early_stop "f1" --label_task "emotion" --num_layers 1 --hidden_size 1536 --epoch_switch 2 --num_encoders 2 --weight_decay 0.01 --learning_rate 0.0001
-    python ../tav_nn.py --fusion "d_c" --sampler "Both_NoAccum"  --seed $i --clip 5  --T_max 3 --epoch 7 --dataset "../../data/more_text_IEMO" --dropout 0.6 --patience 14 --batch_size 24 --early_stop "f1" --label_task "emotion" --num_layers 1 --hidden_size 1536 --epoch_switch 2 --num_encoders 2 --weight_decay 0.01 --learning_rate 0.0001
-    python ../tav_nn.py --fusion "d_c" --sampler "Iter_Accum"  --seed $i --clip 5  --T_max 3 --epoch 7 --dataset "../../data/more_text_IEMO" --dropout 0.6 --patience 14 --batch_size 24 --early_stop "f1" --label_task "emotion" --num_layers 1 --hidden_size 1536 --epoch_switch 2 --num_encoders 2 --weight_decay 0.01 --learning_rate 0.0001
-    python ../tav_nn.py --fusion "d_c" --sampler "Iterative"  --seed $i --clip 5  --T_max 3 --epoch 7 --dataset "../../data/more_text_IEMO" --dropout 0.6 --patience 14 --batch_size 24 --early_stop "f1" --label_task "emotion" --num_layers 1 --hidden_size 1536 --epoch_switch 2 --num_encoders 2 --weight_decay 0.01 --learning_rate 0.0001
-    python ../tav_nn.py --fusion "d_c" --sampler "Weighted"  --seed $i --clip 5  --T_max 3 --epoch 7 --dataset "../../data/more_text_IEMO" --dropout 0.6 --patience 14 --batch_size 24 --early_stop "f1" --label_task "emotion" --num_layers 1 --hidden_size 1536 --epoch_switch 2 --num_encoders 2 --weight_decay 0.01 --learning_rate 0.0001
-done
 
 wandb agent ddi/Must_Tri_Acc_Final/vazpjoco
