cb1a4174b0cea5cbe0f97a7576438bc2e3977019
Pranav Sood <55671093+P-Sood@users.noreply.github.com>
2024-04-03 12:29:05 -0700

    new hparam sets

--- TripleModels/run_zeerak4/urfunny_t_p4.sh
+++ TripleModels/run_zeerak4/urfunny_t_p4.sh
@@ -13,12 +13,12 @@
 # "t_c" 5.5 hours
 
 
-for i in 72
+for i in 149
 do
-    python ../tav_nn.py --fusion "t_p" --sampler "Both"  --seed $i --clip 0.1 --T_max 3 --epoch 11  --dataset "../../data/urfunny" --dropout 0.4  --patience 14 --batch_size 32 --early_stop "acc" --label_task "humour" --num_layers 7 --hidden_size 1536 --epoch_switch 3 --num_encoders 2 --weight_decay 0.001 --learning_rate 0.00001
-    python ../tav_nn.py --fusion "t_p" --sampler "Both_NoAccum"  --seed $i --clip 0.1 --T_max 3 --epoch 11  --dataset "../../data/urfunny" --dropout 0.4  --patience 14 --batch_size 32 --early_stop "acc" --label_task "humour" --num_layers 7 --hidden_size 1536 --epoch_switch 3 --num_encoders 2 --weight_decay 0.001 --learning_rate 0.00001
-    python ../tav_nn.py --fusion "t_p" --sampler "Iter_Accum" --seed $i --clip 0.1 --T_max 3 --epoch 11  --dataset "../../data/urfunny" --dropout 0.4  --patience 14 --batch_size 32 --early_stop "acc" --label_task "humour" --num_layers 7 --hidden_size 1536 --epoch_switch 3 --num_encoders 2 --weight_decay 0.001 --learning_rate 0.00001
-    python ../tav_nn.py --fusion "t_p" --sampler "Iterative" --seed $i --clip 0.1 --T_max 3 --epoch 11  --dataset "../../data/urfunny" --dropout 0.4  --patience 14 --batch_size 32 --early_stop "acc" --label_task "humour" --num_layers 7 --hidden_size 1536 --epoch_switch 3 --num_encoders 2 --weight_decay 0.001 --learning_rate 0.00001
-    python ../tav_nn.py --fusion "t_p" --sampler "Weighted" --seed $i --clip 0.1 --T_max 3 --epoch 11  --dataset "../../data/urfunny" --dropout 0.4  --patience 14 --batch_size 32 --early_stop "acc" --label_task "humour" --num_layers 7 --hidden_size 1536 --epoch_switch 3 --num_encoders 2 --weight_decay 0.001 --learning_rate 0.00001
+    python ../tav_nn.py --fusion "t_p" --sampler "Both"  --seed $i --clip 3.7434379781749527 --T_max 2 --epoch 16 --dataset "../../data/urfunny" --dropout 0.3437044122742988 --patience 14 --batch_size 8 --early_stop "acc" --label_task "humour" --num_layers 11 --hidden_size 1219 --epoch_switch 3 --num_encoders 5 --weight_decay 0.00718668007102603 --learning_rate 0.00003216023106367913
+    python ../tav_nn.py --fusion "t_p" --sampler "Both_NoAccum"  --seed $i --clip 3.7434379781749527 --T_max 2 --epoch 16 --dataset "../../data/urfunny" --dropout 0.3437044122742988 --patience 14 --batch_size 8 --early_stop "acc" --label_task "humour" --num_layers 11 --hidden_size 1219 --epoch_switch 3 --num_encoders 5 --weight_decay 0.00718668007102603 --learning_rate 0.00003216023106367913
+    python ../tav_nn.py --fusion "t_p" --sampler "Iter_Accum" --seed $i --clip 3.7434379781749527 --T_max 2 --epoch 16 --dataset "../../data/urfunny" --dropout 0.3437044122742988 --patience 14 --batch_size 8 --early_stop "acc" --label_task "humour" --num_layers 11 --hidden_size 1219 --epoch_switch 3 --num_encoders 5 --weight_decay 0.00718668007102603 --learning_rate 0.00003216023106367913
+    python ../tav_nn.py --fusion "t_p" --sampler "Iterative" --seed $i --clip 3.7434379781749527 --T_max 2 --epoch 16 --dataset "../../data/urfunny" --dropout 0.3437044122742988 --patience 14 --batch_size 8 --early_stop "acc" --label_task "humour" --num_layers 11 --hidden_size 1219 --epoch_switch 3 --num_encoders 5 --weight_decay 0.00718668007102603 --learning_rate 0.00003216023106367913
+    python ../tav_nn.py --fusion "t_p" --sampler "Weighted" --seed $i --clip 3.7434379781749527 --T_max 2 --epoch 16 --dataset "../../data/urfunny" --dropout 0.3437044122742988 --patience 14 --batch_size 8 --early_stop "acc" --label_task "humour" --num_layers 11 --hidden_size 1219 --epoch_switch 3 --num_encoders 5 --weight_decay 0.00718668007102603 --learning_rate 0.00003216023106367913
 done
 
